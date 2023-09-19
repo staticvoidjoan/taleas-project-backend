5e5a8e1bf60f68e1deb83bba2072d80cd49bdd3e
Joan Shamet <shametijoan@gmail.com>
2023-09-19 15:00:22 +0200

    CDK INIT

---
+++ test/taleas-project-backend.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const TaleasProjectBackend = require('../lib/taleas-project-backend-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/taleas-project-backend-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new TaleasProjectBackend.TaleasProjectBackendStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
