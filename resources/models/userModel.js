8852f2f0f5cc7f17270301569c0d5ae35ada47b5
Selma Lico <selmalico9@gmail.com>
2023-09-19 15:38:55 +0200

    User model, Category model, database config

---
+++ resources/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "users",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    education: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "education"
+    }],
+    experience: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "experience"
+    }],
+    generalSkills: [{
+        type: String,
+    }],
+    languages: [{
+        type: String,
+    }],
+    certifications: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "certifications"
+    }],
+    links: [{
+        type: String,
+    }],
+    profilePhoto: {
+        type: String,
+    }
+  })
+);
+
+module.exports = User;
