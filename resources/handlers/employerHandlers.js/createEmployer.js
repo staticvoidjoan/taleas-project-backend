ff4a39fb06d9813701e8aa4d097b6ab240ac8a5b
Samuel Dervishi <samueldervishi02@gmail.com>
2023-09-20 14:58:37 +0200

    create employer method

---
+++ resources/handlers/employerHandlers.js/createEmployer.js
@@ -0,0 +1,95 @@
+const AWS = require("aws-sdk");
+const mongoose = require("monogoose");
+const User = require("user-model-path");
+const connectDB = require("connectDB-path");
+const Employer = require("./employerModel");
+const MembershipPlan = require("./memberShipModel");
+
+module.exports.createEmployer = async (event) => {
+  console.log("Lambda function invoked");
+
+  try {
+    const data = JSON.parse(event.body);
+    console.log("Received data", data);
+
+    connectDB();
+    console.log("Connected to the database");
+    const { companyName, industry, address, membershipPlan } = data;
+
+    if (!companyName || !industry || !membershipPlan) {
+      console.log("All fields are required");
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error:
+            "All fields are required( company name, industry, address and membership plan)",
+        }),
+      };
+    }
+
+    const nameRegex = /^[A-Za-z\s]+$/;
+    if (!nameRegex.test(companyName)) {
+      console.log("Invalid name format");
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error:
+            "Invalid name format! The company name should only contain letters and spaces",
+        }),
+      };
+    }
+
+    const industryName = /^[A-Za-z\s]+$/;
+    if (!industryName.test(industry)) {
+      console.log("Invalid industry title");
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "Industry should only contain letters and symbols",
+        }),
+      };
+    }
+
+    const membershipPlanExists = await MembershipPlan.findOne({
+      membershipPlan,
+    });
+    if (!membershipPlanExists) {
+      console.log("Membership plan does not exists");
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: "Membership plan does not exists! Please choose another one.",
+        }),
+      };
+    }
+
+    const newEmployer = new Employer({
+      companyName,
+      industry,
+      address,
+      membershipPlan,
+    });
+
+    await newEmployer.save();
+    console.log("Employer saved to the database", newEmployer);
+
+    return {
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({ message: "Company data saved successfully" }),
+    };
+  } catch (error) {
+    console.log("An error happened", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "An error occurred while creating the employer",
+      }),
+    };
+  }
+};
