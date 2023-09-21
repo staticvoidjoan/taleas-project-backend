d181e5a5c618b0d8ceb512e60155c396bca4ca28
Selma Lico <selmalico9@gmail.com>
2023-09-21 09:42:39 +0200

    Like a post

---
+++ resources/handlers/userHandlers/likePost.js
@@ -0,0 +1,41 @@
+const {connectDB} = require('../../config/dbConfig');
+const User = require('../../models/userModel');
+const Post = require('../../models/postModel');
+
+module.exports.likePost = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    await connectDB();
+
+    const email = event.pathParameters.email;
+    const user = await User.findOne({ email: email });
+
+    const postId = event.pathParameters.id;
+    const post = await Post.findById(postId);
+
+    //Update post likedBy array
+    await post.likedBy.push(user);
+
+    return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({ status: "success", message: "Post liked successfully"}),
+      };
+} catch (error) {
+    console.error("Something went wrong", error);
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        status: "error",
+        message: "An error occurred during like",
+      }),
+    };
+  }
+}
