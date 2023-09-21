887ce459761f40f8db2dee26ce19336c48625844
unknown <joanajaupi01@gmail.com>
2023-09-21 16:11:15 +0200

    get all posts based on the activity history of a user

---
+++ resources/handlers/postHandlers/getAllPostsForAUser.js
@@ -0,0 +1,50 @@
+const connectDB = require("../../config/dbConfig");
+const Post = require("../../models/postModel");
+const History = require("../../models/historyModel");
+
+module.exports.getAllPostsForAUser = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectDB();
+    const {id} = event.pathParameters;
+    try {
+
+            const userHistory = History.findOne({user: id});
+            const likedPostIds = userHistory.likedPosts;
+            const dislikedPostIds = userHistory.dislikedPosts;
+
+            // Query for new posts that the user hasn't interacted with
+            const posts = await Post.find({_id: { $nin: [...likedPostIds, ...dislikedPostIds] }});
+
+            if (posts.length === 0) {
+                return {
+                    statusCode: 404, 
+                    headers : {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Credentials": true,
+                    },
+                    body : {
+                        status: "error", 
+                        error: "No posts found"
+                    }
+                }
+            }
+    
+            return {
+                statusCode: 200, 
+                headers : {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                },
+                body : JSON.stringify(posts)
+            }
+    }catch(error) {
+        return {
+            statusCode: 500, 
+            headers : {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body : JSON.stringify(error)
+        }
+    }
+}
