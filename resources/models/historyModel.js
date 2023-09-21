514ba89e98b3508604dc2cbb9aea5c0840b7ea74
unknown <joanajaupi01@gmail.com>
2023-09-21 15:48:31 +0200

    added history model

---
+++ resources/models/historyModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+
+const History = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true,
+        ref: "users"
+    }, 
+    likedPosts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: "Post"
+    }], 
+    dislikedPosts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: "Post"
+    }]
+})
+
+module.exports = mongoose.model("history", History);
