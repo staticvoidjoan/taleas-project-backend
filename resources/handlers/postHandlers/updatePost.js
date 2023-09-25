0e9e6467e556a8bd0a8fe3d5434089eefcca4b8e
unknown <joanajaupi01@gmail.com>
2023-09-25 11:24:18 +0200

    update

--- resources/handlers/postHandlers/updatePost.js
+++ resources/handlers/postHandlers/updatePost.js
@@ -2,113 +2,98 @@ const { connectDB } = require("../../config/dbConfig");
 const Post = require("../../models/postModel");
 const mongoose = require("mongoose");
 
-
 module.exports.updatePost = async (event, context) => {
-
     context.callbackWaitsForEmptyEventLoop = false;
-    await connectDB();
-    try{
-        const creatorId = event.pathParameters.id; 
-        const {id} = event.queryStringParameters;
 
-        if(!mongoose.Types.ObjectId.isValid(id)) {
+    try {
+        await connectDB();
+
+        const { id } = event.queryStringParameters;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
             return {
-                statusCode: 400, 
-                headers : {
+                statusCode: 400,
+                headers: {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Credentials": true,
                 },
-                body : JSON.stringify(
-                    {
-                        status: "error", 
-                        error: "Please provide a valid id"
-                    }
-                )
-            }
+                body: JSON.stringify({
+                    status: "error",
+                    error: "Please provide a valid id",
+                }),
+            };
         }
+
         const postExists = await Post.findById(id);
-        if(!postExists) {
+
+        if (!postExists) {
             return {
                 statusCode: 404,
                 headers: {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Credentials": true,
-                }, 
-                body:JSON.stringify( {
+                },
+                body: JSON.stringify({
                     status: "error",
-                    error: "Post not found"
-                })
-            }
+                    error: "Post not found",
+                }),
+            };
         }
-        const {category, position, requirements, description} = JSON.parse(event.body);
-        if(!category  || !position || !requirements ) {
+
+        const {
+            category,
+            position,
+            requirements,
+            description,
+        } = JSON.parse(event.body);
+
+        if (!category || !position || !requirements) {
             return {
-                statusCode: 400, 
-                headers : {
+                statusCode: 400,
+                headers: {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Credentials": true,
                 },
-                body : JSON.stringify( {
-                    status: "error", 
-                    error: "Please provide all the required fields"
-                })
-            }
+                body: JSON.stringify({
+                    status: "error",
+                    error: "Please provide all the required fields",
+                }),
+            };
         }
-        if(!mongoose.Types.ObjectId.isValid(creatorId)) {
-            return {
-                statusCode: 400, 
-                headers : {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": true,
-                },
-                body : JSON.stringify( {
-                    status: "error", 
-                    error: "Please provide a valid creator id"
-                })
-            }
-        }
-        if(!mongoose.Types.ObjectId.isValid(category)) {
-            return {
-                statusCode: 400, 
-                headers : {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": true,
-                },
-                body : JSON.stringify({
-                    status: "error", 
-                    error: "Please provide a valid category id"
-                })
-            }
-        }
+
+        const { id: creatorId } = postExists; // Use the creatorId from the existing post
 
         const updatedPost = await Post.findByIdAndUpdate(
-            id, 
-            {category, 
-             creatorId, 
-             position, 
-             requirements, 
-             description
-            }, 
-             {new: true});
+            id,
+            {
+                category,
+                creatorId,
+                position,
+                requirements,
+                description,
+            },
+            { new: true }
+        );
 
         return {
-            statusCode: 200, 
-            headers : {
+            statusCode: 200,
+            headers: {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": true,
             },
-            body : JSON.stringify(updatedPost)
-        }
-    }catch(error) {
-
+            body: JSON.stringify(updatedPost),
+        };
+    } catch (error) {
         return {
-            statusCode: 500, 
+            statusCode: 500,
             headers: {
-                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Credentials": true,
-            }, 
-            body: JSON.stringify(error)
-        }
-
+            },
+            body: JSON.stringify({
+                status: "error",
+                error: error.message, // Return the error message
+            }),
+        };
     }
-}
+};
