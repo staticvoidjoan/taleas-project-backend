bc548617433d49d405d9aeaa0c895edd40c8322e
Selma Lico <selmalico9@gmail.com>
2023-10-12 19:01:30 +0200

    Samuel used my laptop for lambda trigger :p

--- resources/handlers/lambdaTriggerHandlers/lambdaTrigger.js
+++ resources/handlers/lambdaTriggerHandlers/lambdaTrigger.js
@@ -9,80 +9,84 @@ module.exports.lambdaTrigger = async (event) => {
   await connectDB();
 
   try {
-    if (event.request.userAttributes.hasOwnProperty("custom:isEmployee")) {
-      var isEmployee = event.request.userAttributes["custom:isEmployee"];
-      if (isEmployee === "true") {
-        var name = event.request.userAttributes["given_name"];
-        var lastname = event.request.userAttributes["family_name"];
-        var email = event.request.userAttributes["email"];
-        var dateOfBirth = event.request.userAttributes["birthdate"];
+    if (event.triggerSource === "PostConfirmation_ConfirmSignUp") {
+      if (event.request.userAttributes.hasOwnProperty("custom:isEmployee")) {
+        var isEmployee = event.request.userAttributes["custom:isEmployee"];
+        if (isEmployee === "true") {
+          var name = event.request.userAttributes["given_name"];
+          var lastname = event.request.userAttributes["family_name"];
+          var email = event.request.userAttributes["email"];
+          var dateOfBirth = event.request.userAttributes["birthdate"];
 
-        // Add your validation logic here
-        const nameRegEx = /^[a-zA-Z\s]{2,30}$/;
-        const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          // Add your validation logic here
+          const nameRegEx = /^[a-zA-Z\s]{2,30}$/;
+          const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if (!nameRegEx.test(name)) {
-          console.log("Name contains invalid characters");
-          throw new Error('Name does not contain valid characters');
+          if (!nameRegEx.test(name)) {
+            console.log("Name contains invalid characters");
+            throw new Error("Name does not contain valid characters");
+          }
+
+          if (!nameRegEx.test(lastname)) {
+            console.log("Lastname contains invalid characters");
+            throw new Error("Lastname does not contain valid characters");
+          }
+
+          if (!emailRegEx.test(email)) {
+            console.log("Email is not valid");
+            throw new Error("Email is not valid");
+          }
+
+          const existingUser = await User.findOne({ email: email });
+          if (existingUser) {
+            console.log("Email already exists");
+            throw new Error("Email already exists");
+          }
+
+          const user = new User({
+            name,
+            lastname,
+            email,
+          });
+
+          await user.save();
+          console.log("User saved to the database", user);
+          console.log("Is mployee: ", isEmployee);
+        } else {
+          var companyName = event.request.userAttributes["given_name"];
+          var email = event.request.userAttributes["email"];
+
+          // Add your validation logic here
+          const nameRegEx = /^[A-Za-z0-9\s.]+$/;
+          const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+          if (!nameRegEx.test(companyName)) {
+            console.log("Company name contains invalid characters");
+            throw new Error("Company name does not contain valid characters");
+          }
+
+          if (!emailRegEx.test(email)) {
+            console.log("Email is not valid");
+            throw new Error("Email is not valid");
+          }
+
+          const existingEmployer = await Employer.findOne({ email: email });
+          if (existingEmployer) {
+            console.log("Email already exists");
+            throw new Error("Email already exists");
+          }
+
+          const newEmployer = new Employer({
+            companyName,
+            email,
+          });
+
+          await newEmployer.save();
+          console.log("Employer saved to the database", newEmployer);
         }
-
-        if (!nameRegEx.test(lastname)) {
-          console.log("Lastname contains invalid characters");
-          throw new Error('Lastname does not contain valid characters');
-        }
-
-        if (!emailRegEx.test(email)) {
-          console.log("Email is not valid");
-          throw new Error('Email is not valid');
-        }
-
-        const existingUser = await User.findOne({ email: email });
-        if (existingUser) {
-          console.log("Email already exists");
-          throw new Error('Email already exists');
-        }
-
-        const user = new User({
-          name,
-          lastname,
-          email,
-        });
-
-        await user.save();
-        console.log("User saved to the database", user);
-        console.log("Is mployee: ", isEmployee);
-      } else {
-        var companyName = event.request.userAttributes["given_name"];
-        var email = event.request.userAttributes["email"];
-
-        // Add your validation logic here
-        const nameRegEx = /^[A-Za-z0-9\s.]+$/;
-        const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (!nameRegEx.test(companyName)) {
-          console.log("Company name contains invalid characters");
-          throw new Error('Company name does not contain valid characters');
-        }
-
-        if (!emailRegEx.test(email)) {
-          console.log("Email is not valid");
-          throw new Error('Email is not valid');
-        }
-
-        const existingEmployer = await Employer.findOne({ email: email });
-        if (existingEmployer) {
-          console.log("Email already exists");
-          throw new Error('Email already exists');
-        }
-
-        const newEmployer = new Employer({
-          companyName,
-          email,
-        });
-
-        await newEmployer.save();
-        console.log("Employer saved to the database", newEmployer);
       }
+    } else if (event.trigger === "PreAuthentication_Authentication") {
+      console.log("Password received");
     }
   } catch (error) {
     console.error("An error happened", error);
