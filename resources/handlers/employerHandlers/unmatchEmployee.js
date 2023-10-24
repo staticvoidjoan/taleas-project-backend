ef4f1100fd81ccc087151d206d30a7296a990243
unknown <joanajaupi01@gmail.com>
2023-10-24 16:08:35 +0200

    update remove a match

--- resources/handlers/employerHandlers/unmatchEmployee.js
+++ resources/handlers/employerHandlers/unmatchEmployee.js
@@ -1,53 +1,33 @@
-
 const Responses = require("../apiResponses");
 const Post = require("../../models/postModel");
-const {connectDB} = require("../../config/dbConfig");
-
+const { connectDB } = require("../../config/dbConfig");
 
 module.exports.unmatchEmployee = async (event) => {
+  try {
+    await connectDB();
 
-    try {
+    const employeerId = event.queryStringParameters.employeerId;
+    const employeeId = event.queryStringParameters.employeeId;
 
-        await connectDB();
+    // Use the updateMany method to remove the employeeId from recLikes in multiple posts
+    const result = await Post.updateMany(
+      { creatorId: employeerId, recLikes: employeeId },
+      { $pull: { recLikes: employeeId } }
+    );
 
-        const employeerId  = event.queryStringParameters.employeerId;
-        const employeeId = event.queryStringParameters.employeeId;
-
-        //find many posts that have the employeeId in the recLikes array and creator id as the employerId
-
-        const posts = await Post.find({ creatorId: employeerId, recLikes: employeeId });
-        if(!posts) {
-            return Responses._400({message: "No posts found"});
-        }
-
-        console.log(posts);
-
-        //remove the employeeId from the recLikes array
-        const updatedPosts = posts.map((post) => ({
-            ...post,
-            recLikes: post.recLikes.filter((id) => id !== employeeId),
-          }));
-
-        console.log(updatedPosts);
-
-        //save the updated posts
-        await Promise.all(updatedPosts.map((post) => post.save()));
-
-
-          
-
-        return Responses._200({
-            status: "success",
-            message: "Employee unmatched successfully",
-        });
-
-        
-
-    }catch(error) {
-        console.error("Something went wrong", error);
-        return Responses._500({
-            status: "error",
-            message: "Something went wrong",
-        });
+    if (result.nModified === 0) {
+      return Responses._400({ message: "No posts found or no changes made" });
     }
-}
+
+    return Responses._200({
+      status: "success",
+      message: "Employee unmatched successfully",
+    });
+  } catch (error) {
+    console.error("Something went wrong", error);
+    return Responses._500({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
