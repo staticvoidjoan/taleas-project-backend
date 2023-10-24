03ec2aec37a44e8ca577a4299811b458e7d3f4a9
unknown <joanajaupi01@gmail.com>
2023-10-24 13:09:22 +0200

    unmatch employee functionality

---
+++ resources/handlers/employerHandlers/unmatchEmployee.js
@@ -0,0 +1,39 @@
+
+const Responses = require("../apiResponses");
+const Post = require("../../models/Post");
+
+module.exports.unmatchEmployee = async (event) => {
+
+    try {
+
+        const employeerId  = event.queryStringParameters.employeerId;
+        const employeeId = event.queryStringParameters.employeeId;
+
+        const post = await Post.findOne({employeerId: employeerId});
+
+        if(!post) {
+            return Responses._404({
+                status: "error",
+                message: "Post is not found anywhere",
+            });
+        }
+
+        if(!post.recLikes.includes(employeeId)) {
+            return Responses._400({
+                status: "error",
+                message: "The user has not liked the post",
+            });
+        }
+
+        await Post.updateOne({ _id: post._id }, { $pull: { recLikes: employeeId } });
+        console.log("User removed from liked array successfully");
+        await post.save();
+
+    }catch(error) {
+        console.error("Something went wrong", error);
+        return Responses._500({
+            status: "error",
+            message: "Something went wrong",
+        });
+    }
+}
