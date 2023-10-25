17987c531942d91ddae5ab59013c98e0df19852a
Joan Shamet <shametijoan@gmail.com>
2023-10-25 15:54:56 +0200

    Commit

--- resources/handlers/contentModeration/analyse.js
+++ resources/handlers/contentModeration/analyse.js
@@ -4,6 +4,8 @@ const filter = require("bad-words");
 const Comprehend = new AWS.Comprehend();
 const profanityFilter = new filter();
 const innapropriateWordSet = require("./innapropriateWordSet.json");
+const Translate = new AWS.Translate();
+
 
 // Populate the filter with words from the "dataset"
 innapropriateWordSet.dataset.forEach(word => {
@@ -20,20 +22,26 @@ exports.handler = async (event) => {
   }
 
   const text = body.text;
- 
-  const params = {
-    LanguageCode: "en",
-    TextList: [text],
-  };
+  //Translate the text
+  const translationParams = {
+    SourceLanguageCode: 'auto',
+    TargetLanguageCode: 'en',
+    Text: text,
+  }
 
+  
   try {
-    const entityResults = await Comprehend.batchDetectEntities(params).promise();
-    const entities = entityResults.ResultList[0];
-
+    const translationResult = await Translate.translateText(translationParams).promise();
+    const translatedText = translationResult.TranslatedText;
+      console.log("Normal tex", text)
+      console.log("Translated text", translatedText)
+    
+      const params = {
+        LanguageCode: "en",
+        TextList: [translatedText],
+      };
     const sentimentResults = await Comprehend.batchDetectSentiment(params).promise();
     const sentiments = sentimentResults.ResultList[0];
-    const textSentiment = await Comprehend.detectSentiment(params);
-
     // Log specific properties without circular references
     console.log('Entities:', entities.Entities);
     console.log('Sentiments:', sentiments.Sentiment, sentiments.SentimentScore);
